65d4b8f19c7e2c8d766f33db0b32dd00cdde8bce
prapti1112 <praptipatil01@gmail.com>
2024-08-05 00:24:37 +0530

    ✏️ Fixed typo in settings.py

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parentgi
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
